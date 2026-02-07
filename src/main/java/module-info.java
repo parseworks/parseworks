b077cc80ca0f0360dc2148bb11262c75d673a2c3
jason bailey <jason.bailey@24601.org>
2026-02-07 08:40:03 -0500

    feat (module)!: removed exposed impl classes

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,4 @@
 module io.github.parseworks {
     exports io.github.parseworks;
-    exports io.github.parseworks.impl;
     exports io.github.parseworks.parsers;
-    exports io.github.parseworks.impl.result;
 }
